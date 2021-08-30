eaf9bbff6c9d6f1c66c63abc8092b3e84315ef4a
mariananogueirab <mariananbp@gmail.com>
2021-08-30 18:39:15 -0300

    Implementando o redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions aqui
+export const TYPE_USER = 'TYPE_USER';
+
+export const setUser = (payload) => ({
+  type: TYPE_USER,
+  payload,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { TYPE_USER } from '../actions';
+
+const initialState = {
+  email: '',
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+  case TYPE_USER:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default user;
